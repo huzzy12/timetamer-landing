36d41b5e33be688dd3896e62e6a86f3b7cad0141
Kamran Madni <huzaifamadni60@gmail.com>
2024-10-09 11:37:24 +0500

    Add Vercel Analytics

---
+++ src/app/layout.js
@@ -0,0 +1,10 @@
+import { Analytics } from '@vercel/analytics/react';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <Analytics />
+    </html>
+  );
+}
